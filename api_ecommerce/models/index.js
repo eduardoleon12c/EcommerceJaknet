5bcfcc18e026fb38fde8c5ec25d4a171639f67a1
EduardoLeonCastillo <eduardoleon1266@gmail.com>
2023-10-02 22:22:03 -0600

    modulos de descuento

--- api_ecommerce/models/index.js
+++ api_ecommerce/models/index.js
@@ -4,11 +4,13 @@ import Product from './Product'
 import Variedad from './Variedad'
 import Slider from './Slider'
 import Cupone from './Cupone'
+import Discount from './Discount'
 export default {
     User,
     Categorie,
     Product,
     Variedad,
     Slider,
-    Cupone
+    Cupone,
+    Discount,
 }
--- api_ecommerce/router/index.js
+++ api_ecommerce/router/index.js
@@ -4,6 +4,7 @@ import Categorie from './Categorie'
 import Product from './Product'
 import Slider from './Slider'
 import Cupone from './Cupone'
+import Discount from './Discount'
 
 const router = routerx();
 router.use('/users', User);
@@ -11,5 +12,6 @@ router.use('/categories', Categorie);
 router.use('/products', Product);
 router.use('/sliders', Slider);
 router.use('/cupones', Cupone);
+router.use('/discount', Discount);
 
 export default router;
